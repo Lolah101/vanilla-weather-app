04aaac3bb819d9fc3d5a79fe42cb6863948ef128
Loza <97474199+Lolah101@users.noreply.github.com>
2023-05-29 10:46:45 +0100

    api update and forecast

--- src/app.js
+++ src/app.js
@@ -13,13 +13,64 @@ function formatDate(timestamp) {
     "Monday",
     "Tuesday",
     "Wednesday",
-    "Thurday",
+    "Thursday",
     "Friday",
     "Saturday",
   ];
   let day = days[date.getDay()];
   // calculate the date
   return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+        
+          <div class="col-2">
+            <div class="weather-forcast-date">${formatDay(
+              forecastDay.time
+            )}</div>
+            <img
+              src=${forecastDay.condition.icon_url}
+              width="55"
+            />
+            <div class="weather-forcast-temperatures">
+              <span class="weather-forcast-temperature-max"> ${Math.round(
+                forecastDay.temperature.maximum
+              )}° </span>
+              <span class="weather-forcast-temperature-min"> ${Math.round(
+                forecastDay.temperature.minimum
+              )}° </span>
+            </div>
+          </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(city) {
+  let apiKey = "5ob762b3bbe4td44d7029eeaf879ff08";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
@@ -32,23 +83,26 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
+  cityElement.innerHTML = response.data.city;
+  celsiusTemperature = response.data.temperature.current;
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+
+  getForecast(response.data.city);
 }
 function search(city) {
-  let apiKey = "0fa6704f3387fb503d492b33889f6cc9";
+  let apiKey = "5ob762b3bbe4td44d7029eeaf879ff08";
 
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0fa6704f3387fb503d492b33889f6cc9&units=metric`;
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiURL).then(displayTemperature);
 }
